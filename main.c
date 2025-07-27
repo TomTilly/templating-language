cdbf119d332eabd40ef500cc63578621f36afee5
Tom Tillistrand <tomtillistrand@gmail.com>
2025-07-26 22:04:08 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  templating-language
+//
+//  Created by Thomas Tillistrand and Thomas Foster on 7/26/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
